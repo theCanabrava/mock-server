4e111d0fd57b9f53db2767f097d01fcac3357f20
Victor Canabrava <victor.canabrava@direcional.com.br>
2021-01-26 08:58:25 -0300

    Engine setup

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const loginRoute = require('./src/routes/login');
 const apiRoute = require('./src/routes/api');
 
 const app = express();
+app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/login', loginRoute);
